8ac7dfb65a72b5d2deeb1bc103aae16e294dfa1d
kesavkumar05 <kesav5801@gmail.com>
2024-02-01 15:26:34 +0530

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './admin/user/user.component';
+import { PostComponent } from './admin/post/post.component';
+import { HomeComponent } from './admin/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path : 'user', component : UserComponent},
+{path : 'post', component : PostComponent},
+{path : 'home', component : HomeComponent}];
